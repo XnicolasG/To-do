41697d53657819d8af6e8192f3e0a80e91ae8536
Nicolás García <nicolasg.ngl@gmail.com>
2026-05-23 22:50:54 -0500

    Update agregar_tareas methos

--- src/App.java
+++ src/App.java
@@ -1,12 +1,11 @@
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    static List<Tarea> tareas = new ArrayList<>();
-    static int contador = 0;
+    static List<Tarea> tareas = obtener_tareas();
+    static int contador = tareas.size();
     static Boolean currentStatus;
 
     public static void main(String[] args) throws Exception {
@@ -16,24 +15,38 @@ public class App {
         do {
             System.out.println("Selecciona que deseas hacer: ");
             System.out.println("1. Agregar una nueva tarea.");
-            if (contador >= 1) {
-                System.out.println("2. Obtener tareas.");
-                System.out.println("3. Mostrar tareas.");
-                System.out.println("4. Completar una tarea.");
-                System.out.println("5. Contar tareas pendientes.");
+            System.out.println("2. Mostrar tareas.");
+            System.out.println("3. Completar una tarea.");
+            System.out.println("4. Contar tareas pendientes.");
+
+            System.out.println("0. Salir.");
+            option = sc.nextInt();
+
+            if (option == 1) {
+                sc.nextLine();
+                System.out.println("Ingrese la nueva tarea. ");
+                String nombreTarea = sc.nextLine();
+                List<Tarea> nuevasTareas = agregar_tareas(tareas, nombreTarea);
+                System.out.println("Se ha registrado: " + nuevasTareas.get(nuevasTareas.size() - 1));
 
             }
-            System.out.println("0. Salir.");
-            option = sc.nextInt();    
-            
-            if(option == 1){
-                agregar_tareas();
-            }
-            if (tareas.size() >=1){
+            if (tareas.size() >= 1) {
                 if (option == 2) {
                     obtener_tareas();
-                }else if (option == 3){
                     mostrar_tareas(tareas);
+                } else if (option == 3) {
+                    System.out.println("Ingresa el numero de la tarea que deseas actualizar");
+                    int tareaSeleccionada = sc.nextInt() - 1;
+                    if (tareaSeleccionada > tareas.size()) {
+                        System.err.println("La tarea " + tareaSeleccionada
+                                + " no existe, intenta de nuevo con un numero valido en la siguiente lista:  ");
+                        mostrar_tareas(tareas);
+                        tareaSeleccionada = sc.nextInt() - 1;
+                    }
+                    marcar_completada(tareas, tareaSeleccionada);
+                } else if (option == 4) {
+                    int pendientes = contar_tareas_pendientes(tareas, 0);
+                    System.out.println("Hay " + pendientes + " tareas pendientes por completar.");
                 }
             }
 
@@ -42,44 +55,76 @@ public class App {
     }
 
     /*
-    Este metodo se encarga de mostrar las tareas con [] si estan pendientes
-    y [X] si estan completadas.
-    */
+     * Metodo encargado de evaluar cuantas tareas estan pendientes por medio de la
+     * lista e indice que se pasan por
+     * medio de parametros. El metodo devuelve 0 en caso el indice sea igual al
+     * largo de la lista.De no ser así
+     * por medio de recursividad se ejecuta la funcion haciendo una suma interta por
+     * medio de iteraciones sobre cada
+     * elemento.
+     */
+    public static int contar_tareas_pendientes(List<Tarea> tareas, int indiceInicial) {
+        if (indiceInicial == tareas.size()) {
+            return 0;
+        }
 
-    public static void mostrar_tareas(List<Tarea> listaTareas){
+        Tarea tareaActual = tareas.get(indiceInicial);
+
+        if (!tareaActual.isComplete) {
+            return 1 + contar_tareas_pendientes(tareas, indiceInicial + 1);
+        } else {
+            return contar_tareas_pendientes(tareas, indiceInicial + 1);
+        }
+    }
+
+    /*
+     * Metodo que actualiza el estado de la tarea
+     */
+    public static void marcar_completada(List<Tarea> listaTareas, int indice) {
+        tareas.get(indice).isComplete = !tareas.get(indice).isComplete;
+        System.out.println(tareas.get(indice));
+    }
+
+    /*
+     * Este metodo se encarga de mostrar las tareas con [] si estan pendientes
+     * y [X] si estan completadas.
+     */
+    public static void mostrar_tareas(List<Tarea> listaTareas) {
 
         for (Tarea tarea : listaTareas) {
-            String checkbox = tarea.isComplete ? "[X]" : "[]" ;
-            System.out.println( (tareas.indexOf(tarea) + 1)+". " + checkbox + " " + tarea.task);
+            String checkbox = tarea.isComplete ? "[X]" : "[]";
+            System.out.println((tareas.indexOf(tarea) + 1) + ". " + checkbox + " " + tarea.task);
         }
     }
 
     /*
-    Metodo que se encarga de mostrar todas las tareas, con la tarea y el estado de esta misma.
-    */
-    public static void obtener_tareas(){
-        System.out.println("Hay " + contador + " tareas agregadas");
-        for (Tarea tarea : tareas) {
-            System.out.println(tarea);
-        }
-    }
-    /*
-    Metodo se encarga de agregar tareas nuevas al arreglo a manera de objetos.
-    se agrega con descripcion de tarea, el status que por defecto sera falso y se crea un ID
-    */
-    public static void agregar_tareas() {
-        Scanner sc = new Scanner(System.in);
+     * Metodo que se encarga de mostrar todas las tareas, con la tarea y el estado
+     * de esta misma.
+     */
+    public static List<Tarea> obtener_tareas() {
+        List<Tarea> listaInicial = new ArrayList<>();
 
-        System.out.println("Ingrese la nueva tarea. ");
-        String nombreTarea = sc.nextLine();
+        listaInicial.add(new Tarea("T1", "Tarea 1", false));
+        listaInicial.add(new Tarea("T2", "Tarea 2", false));
+        listaInicial.add(new Tarea("T3", "Tarea 3", false));
+
+        return listaInicial;
+    }
+
+    /*
+     * Metodo se encarga de agregar tareas nuevas al arreglo a manera de objetos.
+     * se agrega con descripcion de tarea, el status que por defecto sera falso y se
+     * crea un ID
+     */
+
+    // -------------------Pendiente por corregir, debe recibir lista y tarea y
+    // regresar lista modificada
+    public static List<Tarea> agregar_tareas(List<Tarea> listaTareas, String nuevaTarea) {
 
         String id = "T" + contador + 1;
 
-        Tarea nueva = new Tarea(id, nombreTarea, false);
-        tareas.add(nueva);
-        System.out.println("Se ha registrado: " + tareas.get(tareas.size() -1));
-        contador++;
-
+        listaTareas.add(new Tarea(id, nuevaTarea, false));
+        return listaTareas;
     };
 }
 
